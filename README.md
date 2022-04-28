34d110d1817a96593a7ba235b54c3851fa1fa181
郝斌飞 <haobinfei@mininglamp.com>
2022-04-28 14:32:57 +0800

    初始化项目

--- README.md
+++
@@ -1,2 +0,0 @@
-# ingress-manager-operator
-用于自动创建nginx类型ingress的operator
--- main.go
+++ main.go
@@ -1,42 +1,104 @@
+/*
+Copyright 2022 haobinfei.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
-	"log"
+	"flag"
+	"os"
 
-	"github.com/haobinfei/ingress-manager-operator/pkg"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	cloudv1 "github.com/haobinfei/ingress-manager-operator/api/v1"
+	"github.com/haobinfei/ingress-manager-operator/controllers"
+	//+kubebuilder:scaffold:imports
 )
 
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(cloudv1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
+}
+
 func main() {
-	// config
-	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
-
-	if err != nil {
-		inClusterConfig, err := rest.InClusterConfig()
-		if err != nil {
-			log.Fatalln("cat not get config")
-		}
-		config = inClusterConfig
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "a145319e.bigops.com.cn",
+	})
 	if err != nil {
-		log.Fatalln("cat not create client")
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
 	}
 
-	sif := informers.NewSharedInformerFactory(clientset, 0)
-	serviceInformer := sif.Core().V1().Services()
-	ingressInformer := sif.Networking().V1().Ingresses()
+	if err = (&controllers.IngressManagerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "IngressManager")
+		os.Exit(1)
+	}
+	//+kubebuilder:scaffold:builder
 
-	controller := pkg.NewControlle(clientset, serviceInformer, ingressInformer)
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
 
-	stopCh := make(chan struct{})
-	sif.Start(stopCh)
-	sif.WaitForCacheSync(stopCh)
-
-	controller.Run(stopCh)
-
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
